90c09c19fce574febf98fcf51f56121a8093ba40
Alice <alicescarpa@gmail.com>
2014-11-18 12:31:45 -0500

    Now the minutes are actually minutes, instead of seconds

--- pomodoro.py
+++ pomodoro.py
@@ -2,7 +2,7 @@ import time
 import pynotify
 
 def sendmessage(title, message, minutes):
-    time.sleep(minutes)
+    time.sleep(minutes*60)
     pynotify.init("Test")
     notice = pynotify.Notification(title, message)
     notice.show()
