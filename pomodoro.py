2afa8ef2e5eca40631d8a8acda653c7a94bf394f
Alice <alicescarpa@gmail.com>
2014-11-18 12:29:27 -0500

    A testing version of a pomodoro timer, it uses pynotify

---
+++ pomodoro.py
@@ -0,0 +1,12 @@
+import time
+import pynotify
+
+def sendmessage(title, message, minutes):
+    time.sleep(minutes)
+    pynotify.init("Test")
+    notice = pynotify.Notification(title, message)
+    notice.show()
+    return
+
+minutes = int(raw_input("How long? (in minutes) "))
+sendmessage("Time is up!", "Take a 5 minutes break", minutes)
