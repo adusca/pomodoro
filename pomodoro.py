365aba889db771cd19abe248cc47ecff318ffedc
Alice <alicescarpa@gmail.com>
2014-11-19 11:08:14 -0500

    Just making it prettier. You still have to press Ctrl + C to get out

--- pomodoro.py
+++ pomodoro.py
@@ -3,13 +3,16 @@ import pynotify
 
 def sendmessage(title, message, minutes):
     time.sleep(minutes*60)
-    pynotify.init("Test")
+    pynotify.init("Teste")
     notice = pynotify.Notification(title, message)
     notice.show()
     return
 
 minutes = int(raw_input("How many minutes should a pomodoro last? "))
+count = 1
 
 while True:
-    sendmessage("Time is up!", "Take a 5 minutes break", minutes)
+    sendmessage("You did " + str(count) + " pomodoro(s)!", 
+                "Take a 5 minutes break", minutes)
     sendmessage("The break is over!","Time to got back to work", 5)
+    count += 1
